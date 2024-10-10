f515915c1f05ceef4871921f5e6cc52c5800c073
Fennel <fennel@everwild.dev>
2024-10-10 03:56:27 -0400

    finished hw3.1

---
+++ HW3/bookstore.sql
@@ -0,0 +1,161 @@
+use bookstore;
+
+create table author
+(
+    name    varchar(256) primary key,
+    address varchar(256),
+    url     varchar(256)
+);
+
+create table publisher
+(
+    name    varchar(256) primary key,
+    address varchar(256),
+    phone   int,
+    url     varchar(256)
+);
+
+create table customer
+(
+    email   varchar(256) primary key,
+    name    varchar(256),
+    address varchar(256),
+    phone   int
+);
+
+create table shopping_basket
+(
+    basket_id int primary key
+);
+
+create table book
+(
+    isbn  int primary key,
+    title varchar(256),
+    year  int,
+    price float
+);
+
+create table warehouse
+(
+    code    int primary key,
+    address varchar(256),
+    phone   int
+);
+
+create table written_by
+(
+    name varchar(257),
+    isbn int,
+    foreign key (name) references author (name),
+    foreign key (isbn) references book (isbn)
+);
+
+create table published_by
+(
+    name varchar(256),
+    isbn int,
+    foreign key (name) references publisher (name),
+    foreign key (isbn) references book (isbn)
+);
+
+create table basket_of
+(
+    email     varchar(256),
+    basket_id int,
+    foreign key (email) references customer (email),
+    foreign key (basket_id) references shopping_basket (basket_id)
+);
+
+create table contains
+(
+    isbn      int,
+    basket_id int,
+    number    int,
+    foreign key (isbn) references book (isbn),
+    foreign key (basket_id) references shopping_basket (basket_id)
+);
+
+create table stocks
+(
+    isbn   int,
+    code   int,
+    number int,
+    foreign key (isbn) references book (isbn),
+    foreign key (code) references warehouse (code)
+);
+
+create table video
+(
+    video_id int primary key,
+    title    varchar(256),
+    creator  varchar(256)
+);
+
+create table bluray
+(
+    video_id int primary key,
+    price    float,
+    foreign key (video_id) references video (video_id)
+);
+
+create table download
+(
+    video_id int primary key,
+    price    float,
+    foreign key (video_id) references video (video_id)
+);
+
+# Part b
+create table bluray
+(
+    disc_id  int primary key,
+    video_id int,
+    price    float,
+    foreign key (video_id) references video (video_id)
+);
+
+alter table stocks
+    add column disc_id int;
+
+alter table stocks
+    add foreign key (disc_id) references bluray (disc_id);
+# ---------------------------
+drop table bluray;
+drop table download;
+drop table video;
+
+create table video (
+    video_id int primary key,
+    title varchar(256),
+    creator varchar(256),
+    price float
+);
+
+create table bluray (
+    disc_id int primary key,
+    video_id int,
+    price float,
+    foreign key (video_id) references video(video_id)
+);
+
+alter table stocks
+    add foreign key (disc_id) references bluray(disc_id);
+
+create table item (
+    item_id int primary key,
+    isbn int,
+    video_id int,
+    disc_id int,
+    foreign key (isbn) references book(isbn),
+    foreign key (video_id) references video(video_id),
+    foreign key (disc_id) references bluray(disc_id)
+);
+
+drop table item;
+
+alter table contains
+    drop foreign key contains_ibfk_1,
+    drop column isbn,
+    add column item_id int,
+    add foreign key (item_id) references item(item_id);
---
+++ HW3/hw3.typ
@@ -0,0 +1,19 @@
+#set page(height: auto)
+#set text(font: "New Computer Modern")
+
+= Homework 3
+Ethan Meltzer\
+I have adhered to the Honor Code on this assignment.
+
++ \
+  #set enum(numbering: "(a)")
+  + author, name\
+    publisher, name\
+    customer, email\
+    shoppng_basket, basket_id\
+    book, ISBN\
+    warehouse, code
+  + \
+    #image("hw3-1b.png")
+  + \
+    #image("hw3-1c.png")
