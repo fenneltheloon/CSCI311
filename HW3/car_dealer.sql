55aa3022356179be45f98af0b3dd66c432e3e186
Fennel <fennel@everwild.dev>
2024-10-11 02:56:10 -0400

    completed part2

---
+++ HW3/car_dealer.sql
@@ -0,0 +1,72 @@
+use car_dealer;
+
+create table brand
+(
+    brand_id int primary key,
+    name     varchar(256)
+);
+
+create table model
+(
+    model_id int primary key,
+    brand_id int,
+    name     varchar(256),
+    year     int,
+    foreign key (brand_id) references brand (brand_id)
+);
+
+
+create table options
+(
+    option_id   int primary key,
+    model_id    int,
+    name        varchar(256),
+    description text,
+    foreign key (model_id) references model (model_id)
+);
+
+create table dealer
+(
+    dealer_id int primary key,
+    name      varchar(256),
+    location  varchar(256)
+);
+
+create table customer
+(
+    customer_id int primary key,
+    name        varchar(256)
+);
+
+create table car
+(
+    vin      varchar(17) primary key,
+    model_id int,
+    foreign key (model_id) references model (model_id)
+);
+
+create table stocks
+(
+    dealer_id int,
+    vin       varchar(17),
+    foreign key (dealer_id) references dealer (dealer_id),
+    foreign key (vin) references car (vin)
+);
+
+create table owns
+(
+    customer_id int,
+    vin         varchar(17),
+    dealer_id   int,
+    foreign key (customer_id) references customer (customer_id),
+    foreign key (vin) references car (vin),
+    foreign key (dealer_id) references dealer (dealer_id)
+);
+
+create table has_option
+(
+    vin       varchar(17),
+    option_id int,
+    foreign key (vin) references car (vin),
+    foreign key (option_id) references options (option_id)
+);
--- HW3/hw3.typ
+++ HW3/hw3.typ
@@ -10,10 +10,17 @@ I have adhered to the Honor Code on this assignment.
   + author, name\
     publisher, name\
     customer, email\
-    shoppng_basket, basket_id\
+    shopping_basket, basket_id\
     book, ISBN\
     warehouse, code
   + \
     #image("hw3-1b.png")
   + \
     #image("hw3-1c.png")
++ \
+  #image("hw3-2.png")
+  Below is the list of SQL commands used to generate the above ER diagram. The
+  DDL contains both relationship schema as well as primary and foreign key 
+  constraints.
+
+  #raw(read("car_dealer.sql"), lang: "sql")
